ce0714bcf7a59f0863b990b79e4545153b0d2ccf
walkerdrury <walkerdrury@tamu.edu>
2022-10-12 13:43:14 -0500

    transacs

--- sqlFunctions/Main.java
+++ sqlFunctions/Main.java
@@ -8,22 +8,22 @@ public class Main{
 
     static void processTransaction(double cost, int[] items) throws SQLException{
         
-        ResultSet res = Database.executeQuery("SELECT MAX(transactionid) FROM transactions;");
+        ResultSet res = Database.executeQuery("SELECT MAX(transactionid) FROM test_transaction;");
         res.next();
         int t_id = Integer.parseInt(res.getString("max")) +1;
         Database.executeUpdate("INSERT INTO test_transaction VALUES (" + t_id +", CURRENT_DATE, " + cost + ");");
         for(int itemid : items){
-            // INSERT INTO transactionsitems (t_id, itemid)
+            Database.executeUpdate("INSERT INTO transactionitems_test VALUES ("+t_id+", "+itemid+");");
         }
 
     }
 
     public static void main(String[] args){
 
-        int[] a = {};
+        int[] a = {1,2};
         try{
             Database.connect();
-            processTransaction(10, a);
+            processTransaction(11, a);
             Database.disconnect();
         } catch(SQLException e){
             System.out.println(e.getMessage());

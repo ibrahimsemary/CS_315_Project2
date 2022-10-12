28e8b86137edab74c9bf338fb883d9c435db2a4a
ibrahimsemary <ibrahimsemary@tamu.edu>
2022-10-12 15:04:17 -0500

    inserted batch and login functions

--- sqlFunctions/Main.java
+++ sqlFunctions/Main.java
@@ -1,5 +1,7 @@
 import java.sql.*;
 import java.awt.*;
+
+import javax.print.DocFlavor.STRING;
 import javax.swing.*;
 import java.awt.event.*;
 
@@ -17,13 +19,40 @@ public class Main{
         }
 
     }
+    //expiry date should probably be changed to the date orderde or something
+    static void processBatches(int itemId, String expiryDate, int Quantity)throws SQLException{
+        //get the max batch id and add one to it - this is going to be the new primary key
+        //then do the query to put it in the DB
+        ResultSet res = Database.executeQuery("SELECT MAX(batchid) FROM batch;");
+        res.next();
+        int batchId = Integer.parseInt(res.getString("max")) + 1;
+        //Database.executeUpdate("INSERT INTO batch VALUES(" + batchId + "," + itemId + "," + expiryDate + "," + Quantity + ");");
+    }
 
+    static boolean login(String username, String password) throws SQLException{
+        ResultSet res = Database.executeQuery("SELECT * FROM USERS;");
+        res.next();
+        while(res.next()){
+            String tempName = res.getString("name");
+            String tempPassword = res.getString("password");
+            String tempRole = res.getString("Role");
+            if(tempName.equals(username) && password.equals(tempPassword)&& tempRole.equals("manager")){
+                return true;
+                
+            }
+            res.next();
+        }
+        return false;
+
+    }
     public static void main(String[] args){
 
         int[] a = {1,2};
         try{
             Database.connect();
-            processTransaction(11, a);
+            System.out.println(login("miketyson", "password"));
+            System.out.println(login("ibrahim", "haram"));
+
             Database.disconnect();
         } catch(SQLException e){
             System.out.println(e.getMessage());

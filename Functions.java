0425bb3af9d7bde4865a04b535f2a79ef4ef39ca
Ibrahim Semary <92101864+ibrahimsemary@users.noreply.github.com>
2022-10-12 18:17:47 -0500

    removed duplicates + added comments

--- Functions.java
+++ Functions.java
@@ -75,61 +75,8 @@ public class Functions{
     }
 
     /**
-     * @param cost
-     * @param items
+     * @return
      * @throws SQLException
-     */
-    static void processTransaction(double cost, int[] items) throws SQLException{
-        
-        ResultSet res = Database.executeQuery("SELECT MAX(transactionid) FROM test_transaction;");
-        res.next();
-        int t_id = Integer.parseInt(res.getString("max")) +1;
-        Database.executeUpdate("INSERT INTO test_transaction VALUES (" + t_id +", CURRENT_DATE, " + cost + ");");
-        for(int itemid : items){
-            //Database.executeUpdate("INSERT INTO transactionitems_test VALUES ("+t_id+", "+itemid+");");
-        }
-
-    }
-
-    /**
-     * @param itemId
-     * @param expiryDate
-     * @param Quantity
-     * @throws SQLException
-     */
-    //expiry date should probably be changed to the date orderde or something
-    static void processBatches(int itemId, String expiryDate, int Quantity)throws SQLException{
-        //get the max batch id and add one to it - this is going to be the new primary key
-        //then do the query to put it in the DB
-        ResultSet res = Database.executeQuery("SELECT MAX(batchid) FROM batch;");
-        res.next();
-        int batchId = Integer.parseInt(res.getString("max")) + 1;
-        //Database.executeUpdate("INSERT INTO batch VALUES(" + batchId + "," + itemId + "," + expiryDate + "," + Quantity + ");");
-    }
-    /**
-     * @param username
-     * @param password
-     * @return boolean
-     * @throws SQLException
-     */
-    public static boolean login(String username, String password) throws SQLException{
-        ResultSet res = Database.executeQuery("SELECT * FROM USERS;");
-        res.next();
-        while(res.next()){
-            String tempName = res.getString("name");
-            String tempPassword = res.getString("password");
-            String tempRole = res.getString("Role");
-            if(tempName.equals(username) && password.equals(tempPassword)&& tempRole.equals("manager")){
-                return true;
-                
-            }
-            res.next();
-        }
-        return false;
-    }
-    
-    /*
-     * function in order to get all the items in the DB (entrees, sides, add-ons)
      */
     public static ArrayList<Triplet<Integer, String, Double>>getItems() throws SQLException{
        
@@ -146,6 +93,11 @@ public class Functions{
         return items;
     }
 
+    /**
+     * 
+     * @return
+     * @throws SQLException
+     */
     public static ArrayList<Triplet<Integer, String, Double>>getEntrees() throws SQLException{
        
         ArrayList<Triplet<Integer, String, Double>> items = new ArrayList<Triplet<Integer, String, Double>>();
@@ -160,7 +112,11 @@ public class Functions{
         }
         return items;
     }
-
+    /**
+     * 
+     * @return
+     * @throws SQLException
+     */
     public static ArrayList<Triplet<Integer, String, Double>>getSides() throws SQLException{
        
         ArrayList<Triplet<Integer, String, Double>> items = new ArrayList<Triplet<Integer, String, Double>>();
